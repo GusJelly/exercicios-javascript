389d8365fa47727a8f5c53f7c8948f0304f182f8
Gustavo Meireles <gugatpm@gmail.com>
2023-10-21 02:10:15 +0100

    Exercises finished

--- exercises.js
+++ exercises.js
@@ -359,5 +359,43 @@ const exercise_18 = (car_mock_data) => {
 }
 
 const exercise_19 = () => {
+    let result = 0;
+    let numbers = [];
+
+    var car_mock_data = [
+        { "make": "Honda", "model": "Pilot", "year": 2007 },
+        { "make": "Honda", "model": "Fit", "year": 2008 },
+        { "make": "Mazda", "model": "626", "year": 2000 },
+        { "make": "Infiniti", "model": "Q", "year": 2004 },
+        { "make": "Chevrolet", "model": "Express 2500", "year": 2008 },
+        { "make": "Toyota", "model": "Venza", "year": 2009 },
+        { "make": "Maserati", "model": "Quattroporte", "year": 1985 },
+        { "make": "Toyota", "model": "Sequoia", "year": 2003 },
+        { "make": "Suzuki", "model": "XL-7", "year": 2004 },
+        { "make": "GMC", "model": "Yukon XL 1500", "year": 2004 },
+        { "make": "Suzuki", "model": "Swift", "year": 1987 },
+        { "make": "Isuzu", "model": "Ascender", "year": 2005 },
+        { "make": "Toyota", "model": "Camry", "year": 2003 },
+        { "make": "Acura", "model": "Legend", "year": 1987 },
+        { "make": "Pontiac", "model": "Safari", "year": 1987 },
+        { "make": "Austin", "model": "Mini", "year": 1963 }
+    ];
+
+    for (let i = 0; i < car_mock_data.length; i++) {
+        const element = car_mock_data[i];
+        console.log(element.model);
+        // this regexp matches all digits
+        let iterator = element.model.matchAll(/\d+/g);
+
+        for (const current of iterator) {
+            numbers.push(current[0]);
+        }
+    }
+
+    for (let i = 0; i < numbers.length; i++) {
+        result += parseInt(numbers[i]);
+    }
+
+    return result;
 }
 console.log(exercise_19());
